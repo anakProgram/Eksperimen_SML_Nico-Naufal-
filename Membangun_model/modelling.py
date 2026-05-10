8131305c07f5a9a006728f8fe87521707f004f1c
Nico Naufal Arifiyanto <niconovel21@gmail.com>
2026-05-10 10:04:53 +0700

    Fix GitHub Actions workflow path

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -6,19 +6,34 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+# local mlflow database
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
 
-mlflow.sklearn.autolog()
+# create experiment manually
+experiment_name = "Stock_Prediction"
 
-df = pd.read_csv("preprocessing/dataset_preprocessing/clean_data.csv")
+try:
+    experiment_id = mlflow.create_experiment(experiment_name)
+except:
+    experiment = mlflow.get_experiment_by_name(experiment_name)
+    experiment_id = experiment.experiment_id
+
+# load dataset
+df = pd.read_csv(
+    "preprocessing/dataset_preprocessing/clean_data.csv"
+)
 
 X = df.drop("LastPrice", axis=1)
 y = df["LastPrice"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
 )
 
-with mlflow.start_run():
+with mlflow.start_run(experiment_id=experiment_id):
 
     model = RandomForestRegressor()
 
@@ -26,6 +41,13 @@ with mlflow.start_run():
 
     y_pred = model.predict(X_test)
 
-    rmse = mean_squared_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
 
-    print("MSE:", rmse)
+    mlflow.log_metric("mse", mse)
+
+    mlflow.sklearn.log_model(
+        model,
+        artifact_path="model"
+    )
+
+    print("MSE:", mse)
